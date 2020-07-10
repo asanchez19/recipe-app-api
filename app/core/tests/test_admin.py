2dd026b6efbdba295ce69b06603f77b6c5cba12a
Alejandro Sánchez <rasso92@gmail.com>
2020-07-09 23:00:45 -0600

    Add the user admin and its test suite

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,26 @@
+from django.urls import reverse
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+
+
+class AdminSiteTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='asanchez@test.com',
+            password='1231'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='1234',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
