effb49588f146c61ff880b839433bb891e696cc9
Alejandro Sánchez <rasso92@gmail.com>
2020-07-12 09:35:59 -0600

    Add working tag's list

---
+++ app/recipe/urls.py
@@ -0,0 +1,13 @@
+from recipe import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
